14b09b8104ffbdd714870d1e0dc96548433be21d
Florian <florianangels@hotmail.fr>
2022-03-07 17:11:16 +0100

    Setup Java Plugin (Bukkit 1.13)

---
+++ src/main/java/fr/mrunicorn/queuetimemc/QueueTimeMC.java
@@ -0,0 +1,17 @@
+package fr.mrunicorn.queuetimemc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QueueTimeMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
