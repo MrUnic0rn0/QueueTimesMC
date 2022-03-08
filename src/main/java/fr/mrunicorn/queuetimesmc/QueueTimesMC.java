d7b1cbd6a518bc6ffd7c1a8f423ee2a9e849f6b0
MrUnic0rn0 <florianangels@hotmail.fr>
2022-03-08 16:29:33 +0100

    Rmode max_items var in QueueTimesMC

--- src/main/java/fr/mrunicorn/queuetimesmc/QueueTimesMC.java
+++ src/main/java/fr/mrunicorn/queuetimesmc/QueueTimesMC.java
@@ -6,7 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QueueTimesMC extends JavaPlugin {
     private ParkController controller;
-    public final static int max_items = 10;
     private static QueueTimesMC instance;
 
     @Override
